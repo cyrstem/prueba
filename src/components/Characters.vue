35353860da94c1e4a85d237208da57777b2e9894
jacos <cyrstem@gmail.com>
2021-01-08 21:35:57 -0500

    calling api

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,18 +1,21 @@
 <template>
     <div>
         <p>Characters</p>
+        <p>{{characters}}</p>
     </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
 name:'characters',
 data() {
     return {
         characters:[]
     }
-},
-async mounter (){
-    
-    }
- }
+  },
+  mounted() {
+    axios.get('https://rickandmortyapi.com/api/character/')
+    .then(res => (this.characters = res.data))
+  },
+}
 </script>
--- src/components/Episodes.vue
+++ src/components/Episodes.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
         <p>Episodes</p>
+        <p>{{episodes}}</p>
     </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+name:'episodes',
+data() {
+    return {
+        episodes:[]
+    }
+  },
+  mounted() {
+    axios.get('https://rickandmortyapi.com/api/episode/')
+    .then(res => (this.episodes = res.data))
+  },
+}
+</script>
